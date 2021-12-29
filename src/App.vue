4a16f46ef9ac2faa8f91994413fc6b56922e1362
unknown <1019395134@qq.com>
2021-12-29 11:30:33 +0800

    one

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    ssss
+  </div>
+</template>
+
+<style lang="scss">
+html,body,#app{
+  position:relative;
+  width:100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
